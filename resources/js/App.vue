b825a1f814825aa78a3e0b504379d0bc6155ac73
HIROKI MATSUMOTO <hiroki.matsumoto.8986@gmail.com>
2025-03-23 23:11:58 +0900

    Fix: Vue.js and Tailwind CSS configuration

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mx-auto px-4">
+        <h1 class="text-3xl font-bold text-center mt-8">
+            Welcome to Laravel with Vue.js and Tailwind CSS
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script> 
